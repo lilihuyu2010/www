6b7260611942371f89dbddf26d3ffc7e85423dd6
lilihuyu2012 <lili2@foryou56.com>
2017-06-09 17:40:08 +0800

    fixug

--- js/index.js
+++ js/index.js
@@ -1,23 +1,21 @@
-$.function({
-	$("#search-button").off('click').on('click',function(){
-		//获取输入框内容
-		var inputContent = $("#search-input").val().trim();
-		//如果输入框为空
-		if (inputContent == '') {
-			return false;
+$("#search-button").off('click').on('click',function(){
+	//获取输入框内容
+	var inputContent = $("#search-input").val().trim();
+	//如果输入框为空
+	if (inputContent == '') {
+		return false;
+	}
+	//ajax请求接口返回请求结果
+	$.ajax({
+		url : 'http://api.lili.dev/v1/search/index',
+		data : {'input':inputContent,'timestamp':getTimestamp()},
+		type : 'post',
+		dataType : 'json',
+		success : function (result) {
+			console.log(result);
+		},
+		error : function () {
+			alert('出错了！');
 		}
-		//ajax请求接口返回请求结果
-		$.ajax({
-			url : 'api.lili.dev/v1/search/index',
-			data : {'input':inputContent,'timestamp':getTimestamp()},
-			type : 'post',
-			dataType : 'json',
-			success : function (result) {
-				console.log(result);
-			},
-			error : function () {
-				alert('出错了！');
-			}
-		});
 	});
 });
